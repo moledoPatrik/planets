44d105aa5b65e9faaa2b1d0d267bdee8217a9e46
Patrik Moledo <moledop55@gmail.com>
2022-12-22 21:38:23 +0900

    alien new and create, planet button to trigger it

--- app/controllers/aliens_controller.rb
+++ app/controllers/aliens_controller.rb
@@ -1,22 +1,39 @@
 class AliensController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  # def index
+  # end
+  # def show
+  # end
 
   def new
+    @alien = Alien.new
+    @planet = Planet.find(params[:planet_id])
   end
 
   def create
+    @planet = Planet.find(params[:planet_id])
+    @alien = Alien.new(alien_params)
+    @alien.planet = @planet
+    if @alien.save
+      redirect_to @planet
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
   def destroy
+    raise
+    @alien =
+  end
+
+  private
+
+  def alien_params
+    params.require(:alien).permit(:name, :age, :photo)
   end
 end
