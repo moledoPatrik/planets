005058517f8199a71d88910342b7d289441b6bcb
Patrik Moledo <moledop55@gmail.com>
2023-01-09 14:52:58 +0900

    first model test

--- spec/models/alien_spec.rb
+++ spec/models/alien_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Alien, "#increment_age!" do
-  it "increments age" do
+  it "increments alien's age" do
     alien = build(:alien, age: 20)
     alien.increment_age!
     expect(alien.age).to eq 21
